a09459f669b6392e153a0eed82b242e2f5abeecc
Rafael Cardenas <rafael@rafaelcr.com>
2017-03-15 12:19:39 -0600

    Start parsing real responses

--- lib/luis.rb
+++ lib/luis.rb
@@ -1,11 +1,54 @@
+require "json"
+require "rest-client"
+
 require "luis/version"
 require "luis/entity"
 require "luis/intent"
 
 module Luis
 
-  def self.query(utterance)
+  class Result
+    attr_reader :query, :intents, :entities
 
+    def initialize(query, intents, entities)
+      @query = query
+      @intents = intents
+      @entities = entities
+    end
+
+    def top_scoring_intent
+      @intents.first
+    end
+  end
+
+  def self.query(utterance)
+    endpoint = ENV["LUIS_ENDPOINT_URL"]
+    raise "#{self} requires an endpoint url" if endpoint == nil
+
+    url = "#{endpoint}#{utterance}"
+    result = RestClient.get(url)
+    json = JSON.parse(result)
+    parse_response(json)
+  end
+
+  private
+
+  def self.parse_response(response)
+    raise "#{self} received a nil response" if response == nil
+
+    query = response["query"]
+
+    intents = [ Intent.new(response["topScoringIntent"]) ]
+    for intent in response["intents"][1..-1] do  # The first is always the top scoring intent.
+      intents.push(Intent.new(intent))
+    end
+
+    entities = []
+    for entity in response["entities"] do
+      entities.push(Entity.new(entity))
+    end
+
+    Result.new(query, intents, entities)
   end
 
 end
--- spec/luis_spec.rb
+++ spec/luis_spec.rb
@@ -1,11 +1,60 @@
 require "spec_helper"
 
 describe Luis do
+  let(:payload) {
+    {
+      "query" => "Quiero pagar tenencia",
+      "topScoringIntent" => {
+        "intent" => "select service",
+        "score" => 0.9999995
+      },
+      "intents" => [
+        {
+          "intent" => "select service",
+          "score" => 0.9999995
+        },
+        {
+          "intent" => "None",
+          "score" => 0.0412132144
+        }
+      ],
+      "entities" => [
+        {
+          "entity" => "tenencia",
+          "type" => "service",
+          "startIndex" => 13,
+          "endIndex" => 20,
+          "score" => 0.916118264
+        },
+        {
+          "entity" => "quiero",
+          "type" => "need",
+          "startIndex" => 0,
+          "endIndex" => 5,
+          "score" => 0.954107046
+        },
+        {
+          "entity" => "pagar",
+          "type" => "verb",
+          "startIndex" => 7,
+          "endIndex" => 11,
+          "score" => 0.959142148
+        }
+      ]
+    }
+  }
+
   it "has a version number" do
     expect(Luis::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "parses a query response correctly" do
+    response = nil
+    expect { response = Luis.parse_response(payload) }.not_to raise_error
+    expect(response).not_to be nil
+    expect(response.query).to eq("Quiero pagar tenencia")
+    expect(response.intents.length).to eq(2)
+    expect(response.top_scoring_intent.name).to eq("select service")
+    expect(response.entities.length).to eq(3)
   end
 end
