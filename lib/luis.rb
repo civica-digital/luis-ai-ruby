496632b4165d2625b8e9488928bc1381c8b5b655
Rafael Cárdenas <rafael@rafaelcr.com>
2017-03-16 12:45:27 -0600

    Allow configuration through a yielded config block (#1)

--- lib/luis.rb
+++ lib/luis.rb
@@ -1,6 +1,7 @@
 require "json"
 require "rest-client"
 
+require "luis/configuration"
 require "luis/version"
 require "luis/entity"
 require "luis/intent"
@@ -11,9 +12,9 @@ module Luis
     attr_reader :query, :intents, :entities
 
     def initialize(query, intents, entities)
-      @query = query
-      @intents = intents
-      @entities = entities
+      @query = query || :no_query_provided
+      @intents = intents || []
+      @entities = entities || []
     end
 
     def top_scoring_intent
@@ -21,8 +22,24 @@ module Luis
     end
   end
 
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
   def self.query(utterance)
-    endpoint = ENV["LUIS_ENDPOINT_URL"]
+    endpoint = configuration.endpoint_url
     raise "#{self} requires an endpoint url" if endpoint == nil
 
     url = "#{endpoint}#{utterance}"
@@ -39,8 +56,10 @@ module Luis
     query = response["query"]
 
     intents = [ Intent.new(response["topScoringIntent"]) ]
-    for intent in response["intents"][1..-1] do  # The first is always the top scoring intent.
-      intents.push(Intent.new(intent))
+    if response.has_key?("intents")
+      for intent in response["intents"][1..-1] do  # The first is always the top scoring intent.
+        intents.push(Intent.new(intent))
+      end
     end
 
     entities = []
---
+++ lib/luis/configuration.rb
@@ -0,0 +1,9 @@
+module Luis
+  class Configuration
+    attr_accessor :endpoint_url
+
+    def initialize
+      @endpoint_url = nil
+    end
+  end
+end
