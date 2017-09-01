9b3e003b1160939f2e1c82bc5e97e3dea9da252c
Rafael Cárdenas <rafael@rafaelcr.com>
2017-09-01 12:08:39 -0500

    Configure Luis timeout (#8)

--- lib/luis/configuration.rb
+++ lib/luis/configuration.rb
@@ -1,9 +1,11 @@
 module Luis
   class Configuration
     attr_accessor :endpoint_url
+    attr_accessor :timeout
 
     def initialize
       @endpoint_url = nil
+      @timeout = 20
     end
   end
 end
